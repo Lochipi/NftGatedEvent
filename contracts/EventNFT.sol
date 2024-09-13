5401e3d908cbdfd594a81a0bee6b4e9a1dfd28c3
Emase <corneliuslochipi@gmail.com>
2024-09-13 16:48:05 +0300

    update: mintnft function

--- contracts/EventNFT.sol
+++ contracts/EventNFT.sol
@@ -4,19 +4,17 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EventNFT is ERC721, Ownable(msg.sender) {
-    uint256 public _nextEventId;
+contract EventNFT is ERC721, Ownable(msg.sender) { 
 
     mapping(address => bool) public hasMinted;
 
     constructor() ERC721("EventNFT", "ENFT") {}
 
-    function mintNft() external {
+    function mintNft(uint256 _eventId) external {
         require(!hasMinted[msg.sender], "You have already minted the NFT");
 
-        _safeMint(msg.sender, _nextEventId);
-        hasMinted[msg.sender] = true;
-        _nextEventId++;
+        _safeMint(msg.sender, _eventId);
+        hasMinted[msg.sender] = true; 
     }
 
 }
